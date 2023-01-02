3c0f669f1bdb8f6f9538b23972eb7ca1e2ad1215
taegyung <taegyung.e@gmail.com>
2023-01-02 16:07:07 +0900

    게임종료

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@
     // 스테이지마다 랜덤색상 출력 rgb로 출력
     // 다른 색상 표시(난이도에 따라 rgb 색상 조절);
     // 아이템 셔플
-    // 다른 색상의 아이템의 자리 기억해서 그 자리 피해서 표시해주기
     // 스테이지마다 개수 증가
     const stage = document.querySelector('.stage');
     const stageWidth = stage.clientWidth;
@@ -19,26 +18,26 @@
         {level: 2, step: 120, grid: 2},
         {level: 3, step: 65, grid: 3},
         {level: 4, step: 65, grid: 3},
-        {level: 5, step: 55, grid: 3},
-        {level: 6, step: 55, grid: 4},
-        {level: 7, step: 50, grid: 4},
-        {level: 8, step: 50, grid: 4},
-        {level: 9, step: 50, grid: 4},
-        {level: 10, step: 45, grid: 4},
-        {level: 11, step: 45, grid: 5},
-        {level: 12, step: 42, grid: 5},
-        {level: 13, step: 42, grid: 5},
-        {level: 14, step: 40, grid: 5},
-        {level: 15, step: 40, grid: 5},
-        {level: 16, step: 38, grid: 6},
-        {level: 17, step: 38, grid: 6},
-        {level: 18, step: 35, grid: 6},
-        {level: 19, step: 35, grid: 6},
-        {level: 20, step: 35, grid: 6},
-        {level: 21, step: 30, grid: 7},
-        {level: 22, step: 30, grid: 7},
-        {level: 23, step: 25, grid: 7},
-        {level: 24, step: 25, grid: 7},
+        {level: 5, step: 50, grid: 3},
+        {level: 6, step: 50, grid: 4},
+        {level: 7, step: 45, grid: 4},
+        {level: 8, step: 45, grid: 4},
+        {level: 9, step: 40, grid: 4},
+        {level: 10, step: 40, grid: 4},
+        {level: 11, step: 38, grid: 5},
+        {level: 12, step: 38, grid: 5},
+        {level: 13, step: 38, grid: 5},
+        {level: 14, step: 35, grid: 5},
+        {level: 15, step: 35, grid: 5},
+        {level: 16, step: 32, grid: 6},
+        {level: 17, step: 32, grid: 6},
+        {level: 18, step: 30, grid: 6},
+        {level: 19, step: 30, grid: 6},
+        {level: 20, step: 30, grid: 6},
+        {level: 21, step: 28, grid: 7},
+        {level: 22, step: 28, grid: 7},
+        {level: 23, step: 20, grid: 7},
+        {level: 24, step: 20, grid: 7},
         {level: 25, step: 20, grid: 7},
         {level: 26, step: 20, grid: 8},
         {level: 27, step: 15, grid: 8},
@@ -53,25 +52,11 @@
     let otherIndex;
     let isStart = false;
 
-
-    // flow
-    // 메인 색상을 뽑는다.
-    // 아이템의 정보를 grid * grid의 개수만큼 배열에 담는다
-    // otherIndex를 출력해서 저장하고, 
-    // 아이템을 그려준다
-    // 아이템에 색상을 입혀준다
-
-    // 1~30
-    // 2 x 2
-    // 3 x 3
-    // 4 x 4
-    // 
-
     init();
     
     function init() {
         isStart = true;
-        setGame()
+        setGame();
     }
 
     function setGame() {
@@ -82,10 +67,12 @@
         const rgbValue = `rgb(${mainColor[0]}, ${mainColor[1]}, ${mainColor[2]})`;
         const otherRgbValue = `rgb(${otherColor[0]}, ${otherColor[1]}, ${otherColor[2]})`;
         otherIndex = Math.floor(Math.random() * (levelInfo[level].grid * levelInfo[level].grid));
+
         items = Array.from({length: levelInfo[level].grid * levelInfo[level].grid}).fill(rgbValue);
         items[otherIndex] = otherRgbValue;
         levelEl.innerHTML = level + 1;
         timeEl.innerHTML = gameTime;
+        timeEl.parentNode.style.color = 'black'
 
         // 피스생성
         items.forEach(ele => { createItem(ele) });
@@ -121,6 +108,44 @@
         minusValue = [...changeOtherColor].sort((a, b) => a - b)[0];
         return changeOtherColor.map(ele => ele + Math.abs(minusValue) + 2);
     }
+    
+    function timeStart() {
+        timeInterval = setInterval(function(){
+
+            gameTime--;
+            timeEl.innerHTML = (gameTime < 0) ? 0 : gameTime;
+
+            timeEl.parentNode.style.color = (gameTime > 5) ? 'black' : 'red';
+
+            // 5초 남았을때 5초 남았다는 애니메이션 추가
+            if(gameTime <= 5) {
+                timeEl.parentNode.classList.add('bounce');
+                setTimeout(function(){
+                    timeEl.parentNode.classList.remove('bounce');
+                }, 400);
+            }
+
+            if(gameTime <= 0) {
+                gameEnd();
+            }
+        }, 1000);
+    }
+
+    function gameEnd() {
+        const wrap = document.getElementById('wrap');
+        const endEl = document.createElement('div');
+        endEl.classList.add('game_end');
+        endEl.innerHTML = `<span>g</span><span>a</span><span>m</span><span>e</span><span></span><span>e</span><span>n</span><span>d</span>`;
+
+        clearInterval(timeInterval);
+        isStart = false;
+
+        wrap.appendChild(endEl);
+        setTimeout(function(){
+            endEl.remove();
+        }, 6000);
+    }
+
 
     stage.addEventListener('click', function(e){
         if(!isStart) return;
@@ -129,35 +154,24 @@
         const selectItemIdx = ItemAll.findIndex(ele => ele == e.target.closest('.item'));
         
         if(timeInterval == undefined) timeStart();
-        if(level == 29) {
-            clearInterval(timeInterval);
-            isStart = false;
+        if(level === 29) { // 게임 종료
+            gameEnd();
             return;
         }
 
         // 정답 선택시
         if(selectItemIdx === otherIndex) {
             //타임 리셋, 레벨업, 
+            clearInterval(timeInterval);
             stage.innerHTML = '';
             level++;
             gameTime = 15;
             setGame();
+            timeStart();
         } else {
             gameTime -= 3;
         }
     })
-
-    function timeStart() {
-        timeInterval = setInterval(function(){
-            gameTime--;
-            timeEl.innerHTML = (gameTime < 0) ? 0 : gameTime;
-
-            if(gameTime <= 0) {
-                clearInterval(timeInterval);
-                isStart = false;
-            }
-        }, 1000);
-    }
 
 
 })()
